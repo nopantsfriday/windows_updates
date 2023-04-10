bd486db7bc42c91dd2f717278d5aa0b381d28ba6
nopantsfriday <60006702+nopantsfriday@users.noreply.github.com>
2023-04-10 11:47:27 +0200

    Moved winget and UWP apps to the top

--- windows_updates.ps1
+++ windows_updates.ps1
@@ -1,4 +1,4 @@
-# Start PowerShell script with elevated priviliges
+# Start PowerShell with elevated priviliges
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process "wt.exe" "powershell -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 # Check if PSWindowsUpdate is installed
@@ -7,15 +7,15 @@ if (!(Test-Path "$Env:Programfiles\WindowsPowerShell\Modules\PSWindowsUpdate")) 
     Install-Module -Name PSWindowsUpdate -Force
 }
 
-Write-Host "Searching for Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
-Get-WindowsUpdate
-Write-Host "Installing Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
-Install-WindowsUpdate -AcceptAll -Install | Out-Null
 Write-Host "Updating winget apps" -ForegroundColor Cyan -BackgroundColor Black
 winget upgrade --all --silent --include-unknown
 #winget source reset --force
 Write-Host "Updating Windows Store Apps" -ForegroundColor Cyan -BackgroundColor Black
 Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod | Out-Null
+Write-Host "Searching for Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
+Get-WindowsUpdate
+Write-Host "Installing Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
+Install-WindowsUpdate -AcceptAll -Install | Out-Null
 
 function keypress_wait {
     param (
