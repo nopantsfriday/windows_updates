62fa0c9484511510cbe72a6fb6646efe9591ad82
nopantsfriday <jens@nopantsfriday.de>
2023-08-15 19:25:51 +0200

    check if script has already run today

--- windows_updates.ps1
+++ windows_updates.ps1
@@ -1,33 +1,50 @@
-# Start PowerShell with elevated priviliges
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process "wt.exe" "powershell -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+#check if script has already run today
 
-# Check if PSWindowsUpdate is installed
-if (!(Test-Path "$Env:Programfiles\WindowsPowerShell\Modules\PSWindowsUpdate")) {
-    Write-Host "Installing Module PSWindowsUpdate" -ForegroundColor Yellow -BackgroundColor Black 
-    Install-Module -Name PSWindowsUpdate -Force
-}
+if ((Get-ItemProperty "HKLM:\" -name "WindowsUpdatesLastCheck" | Select-Object -exp "WindowsUpdatesLastCheck") -ne (Get-Date -Format "yyyy-MM-dd") ) {
 
-Write-Host "Updating winget apps" -ForegroundColor Cyan -BackgroundColor Black
-winget upgrade --all --silent --include-unknown
-#winget source reset --force
-Write-Host "Updating Windows Store Apps" -ForegroundColor Cyan -BackgroundColor Black
-Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod | Out-Null
-Write-Host "Searching for Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
-Get-WindowsUpdate
-Write-Host "Installing Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
-Install-WindowsUpdate -AcceptAll -Install | Out-Null
+    # Start PowerShell with elevated priviliges
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process "wt.exe" "powershell -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-function keypress_wait {
-    param (
-        [int]$seconds = 60
-    )
-    $loops = $seconds * 10
-    Write-Host "Press any key to exit. (Window will automatically close in $seconds seconds.)" -ForegroundColor Yellow
-    for ($i = 0; $i -le $loops; $i++) {
-        if ([Console]::KeyAvailable) { break; }
-        Start-Sleep -Milliseconds 100
+    # Check if PSWindowsUpdate is installed
+    if (!(Test-Path "$Env:Programfiles\WindowsPowerShell\Modules\PSWindowsUpdate")) {
+        Write-Host "Installing Module PSWindowsUpdate" -ForegroundColor Yellow -BackgroundColor Black 
+        Install-Module -Name PSWindowsUpdate -Force
     }
-    if ([Console]::KeyAvailable) { return [Console]::ReadKey($true); }
-    else { return $null ; }
+
+    Write-Host "Updating winget apps" -ForegroundColor Cyan -BackgroundColor Black
+    winget upgrade --all --silent --include-unknown
+    #winget source reset --force
+    Write-Host "Updating Windows Store Apps" -ForegroundColor Cyan -BackgroundColor Black
+    Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod | Out-Null
+    Write-Host "Searching for Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
+    Get-WindowsUpdate
+    Write-Host "Installing Windows Updates" -ForegroundColor Cyan -BackgroundColor Black
+    Install-WindowsUpdate -AcceptAll -Install | Out-Null
+
+    #Create registry file after script has run
+    $WindowsUpdatesLastCheckDate = (Get-Date -Format "yyyy-MM-dd")
+
+    if (!(Test-Path "HKLM:\")) {
+        New-Item -Path "HKLM:\" -Force | Out-Null
+        New-ItemProperty -Name "WindowsUpdatesLastCheck" -Path "HKLM:\" -Force -PropertyType "String" -Value $WindowsUpdatesLastCheckDate | Out-Null
+    }
+    else {
+        New-ItemProperty -Name "WindowsUpdatesLastCheck" -Path "HKLM:\" -Force -PropertyType "String" -Value $WindowsUpdatesLastCheckDate | Out-Null | Out-Null
+    }
+    function keypress_wait {
+        param (
+            [int]$seconds = 60
+        )
+        $loops = $seconds * 10
+        Write-Host "Press any key to exit. (Window will automatically close in $seconds seconds.)" -ForegroundColor Yellow
+        for ($i = 0; $i -le $loops; $i++) {
+            if ([Console]::KeyAvailable) { break; }
+            Start-Sleep -Milliseconds 100
+        }
+        if ([Console]::KeyAvailable) { return [Console]::ReadKey($true); }
+        else { return $null ; }
+    }
+    keypress_wait
 }
-keypress_wait
+
+else {}
